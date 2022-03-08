63972d1888efd77d73f92253139b49eda296844a
Rizki Ramadhan <rzk.ramadhan@gmail.com>
2022-03-08 17:59:23 +0700

    Update Halaman Homepage

--- index.php
+++ index.php
@@ -4,20 +4,37 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bukit Asam - Fleet's Availability</title>
+    <title>Bukit Asam - Homepage Monitoring System</title>
     <link rel="stylesheet" href="assets/styles/index.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="icon" href="assets/logo/icon.svg" />
 </head>
 <body>
-    <header>
+    <header style="">
+        <figure >
+            <img class="brandptba" src="assets/logo/logoptba.png" alt="brandptba">
+        </figure>
         <h1>Bukit Asam Fleet Availability</h1>
     </header>
-   <div>
-
+    <div>
+        <div class="content">
+            <figure>
+                <img src="assets/illus/report.svg" alt="reportillustration">
+            </figure>
+            <figcaption>
+                <h2>Monitor All <span style="color: red;">Unit Availability</span></h2>
+                <p>Report of Availability Unit, Status, and Fleet Information</p>
+                <a target="_blank" href="pages/fleetSummary.php" class="btngo"><i class="fa fa-paper-plane" style="margin-inline-end: 10px"></i>Check Report</a>
+            </figcaption>
+        </div>
+        <div class="contentlogin">
+            <h2>Maintenance Availability Unit and Fleet Setting </h2>
+            <p>Already have access as an Administrator? We hope you'll be Logged In soon.</p>
+            <a target="_blank" href="pages/login.php" class="btnlogin"><i class="fa fa-sign-in-alt" style="margin-inline-end: 10px"></i>Get Logged In</a>
+        </div>
    </div>
-   <footer>
-       <p>Copyright &copy; 2022 PT. Bukit Asam (Persero) Tbk. | Illustration By Storyset</p>
+   <footer style="background: var(--blue2); margin-block-start: 0 !important;">
+       <p>Copyright &copy; 2022 PT. Bukit Asam (Persero) Tbk. | <a href="https://storyset.com/work"> Illustrations by Storyset</a></p>
    </footer>
 </body>
 </html>
